650a5d55a9845b73dcbf1c8ee45cd1a7cf3eaab3
Adrian <106486758+ovqt@users.noreply.github.com>
2023-10-09 14:04:38 +0200

    Freie Antworten
    Nicht fertig

---
+++ freieantwort.py
@@ -0,0 +1,13 @@
+"""freie Antworten"""
+
+class freieantwort
+
+check_freieantwort
+    answer = input("What is your answer")
+    if answer True:
+        break
+        else:
+            answer.isnotstring or answer.isnotdigit
+            print("Bitte geben sie geben sie eine Valide Eingabe")
+
+def main():
