41a16afae87cbee503e6faae1cbd0c728a748a19
Adrian <106486758+ovqt@users.noreply.github.com>
2023-10-09 15:59:33 +0200

    Update freieantwort.py

--- freieantwort.py
+++ freieantwort.py
@@ -1,37 +1,42 @@
+import random
+
 class FreieAntwort:
     def __init__(self):
         self.answer = ""
 
-def questions():
-    answer1 = input("Wofür steht OOP? ")
-    answer2 = input("Was ist ein String? ")
-    return answer1, answer2 
+
+def get_random_question():
+    questions = [
+        ("Wofür steht OOP? ", "objektorientierte programmierung"),
+        ("Was ist ein String? ", "zeichenkette"),
+        ("Was ist ein Integer", "Ganzahl")
+    ]
+    return random.choice(questions)
+
 
 def check_freieantwort():
     while True:
         answer = input("Was ist deine Antwort: ")
-        if answer.strip() and not answer.isdigit():  # Use parentheses to call isdigit
+        if answer.strip() and not answer.isdigit():
             return answer
         else:
             print("Bitte gib eine valide Antwort ein.")
 
-def check_answer(answer1, answer2):
-    if answer1.lower() == "objektorientierte programmierung" and answer2.lower() == "zeichenkette":
-        return True
-    else:
-        return False
 
 def main():
     print("Hallo Alice. Hier kannst du freie Antworten geben.")
-    answer1, answer2 = questions()
+    question, right_answer = get_random_question()
+    print(question)
+
     freie_antwort = FreieAntwort()
     freie_antwort.answer = check_freieantwort()
     print("Deine Antwort: " + freie_antwort.answer)
 
-    if check_answer(answer1, answer2):
-        print("Deine Antworten sind korrekt!")
+    if freie_antwort.answer.lower() == right_answer:
+        print("Deine Antwort ist korrekt!")
     else:
-        print("Deine Antworten sind nicht korrekt.")
+        print("Deine Antwort ist nicht korrekt. Die richtige Antwort ist:", right_answer)
+
 
 if __name__ == "__main__":
     main()
