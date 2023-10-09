1a75b5bd759eb52fa19131065b6843b3f504fdd4
Adrian <106486758+ovqt@users.noreply.github.com>
2023-10-09 14:13:34 +0200

    Update freieantwort.py
    Nicht fertig jetzt

--- freieantwort.py
+++ freieantwort.py
@@ -1,13 +1,20 @@
-"""freie Antworten"""
+class FreieAntwort:
 
-class freieantwort
+    def __init__(self):
+        self.answer = ""
 
-check_freieantwort
-    answer = input("What is your answer")
-    if answer True:
-        break
+def check_freieantwort():
+    while True:
+        answer = input("What is your answer: ")
+        if answer:
+            break
+        elif not answer.isalnum():
+            print("Bitte geben Sie eine valide Eingabe ein.")
         else:
-            answer.isnotstring or answer.isnotdigit
-            print("Bitte geben sie geben sie eine Valide Eingabe")
+            print("Bitte geben Sie etwas ein.")
 
 def main():
+    check_freieantwort()
+
+if __name__ == "__main__":
+    main()
