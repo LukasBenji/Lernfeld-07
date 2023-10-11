b8f254922f78cacbe2827d4a5e5dd68c709ff78b
Adrian <106486758+ovqt@users.noreply.github.com>
2023-10-11 12:53:40 +0200

    Update freieantwort.py

--- freieantwort.py
+++ freieantwort.py
@@ -1,3 +1,48 @@
+import random
+
+
+class Frage:
+
+    def __init__(self, frage, antwort):
+        self.frage = frage
+        self.antwort = antwort
+
+
+class FreiesAntwortSpiel:
+
+    def __init__(self):
+        self.frage_liste = []
+        self.selection = Selection()
+
+    def add_question(self, frage, antwort):
+        self.frage_liste.append(Frage(frage, antwort))
+
+    def get_random_question(self):
+        return random.choice(self.frage_liste)
+
+    def spielen(self):
+        print("Hallo Alice. Hier kannst du freie Antworten geben.")
+        fragen = self.get_random_question()
+        print(fragen.frage)
+        antwort = input("Was ist deine Antwort: ")
+
+        antwort = self.check_freieantwort(antwort)
+        print("Deine Antwort: " + antwort)
+
+        if antwort.lower() == fragen.antwort:
+            print("Antwort ist korrekt!")
+        else:
+            print("Falsche Antwort. Die richtige Antwort ist:", fragen.antwort)
+
+    def check_freieantwort(self, antwort):
+        if antwort.strip() and not antwort.isdigit():
+            return antwort
+        else:
+            return "Bitte gib eine valide Antwort ein."
+
+
+class Selection:
+
     def __init__(self):
         self.__option_hover = -1
         self.__option_max = 1
@@ -29,3 +74,11 @@
     def reset_selection(self):
         self.__option_hover = -1
         self.__option_selected = -1
+
+
+if __name__ == "__main__":
+    spiel = FreiesAntwortSpiel()
+    spiel.add_question("Wofür steht OOP? ", "objektorientierte programmierung")
+    spiel.add_question("Was ist ein String? ", "zeichenkette")
+    spiel.add_question("Was ist ein Integer? ", "Ganzahl")
+    spiel.spielen()
