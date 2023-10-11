7d85b24df7f9006c410956dba8904fec98f2f652
Adrian <106486758+ovqt@users.noreply.github.com>
2023-10-11 12:50:40 +0200

    Update freieantwort.py

--- freieantwort.py
+++ freieantwort.py
@@ -1,39 +1,31 @@
-import random
-
-class Frage:
-    def __init__(self, frage, antwort):
-        self.frage = frage
-        self.antwort = antwort
-
-class FreiesAntwortSpiel:
     def __init__(self):
-        self.frage_liste = []
-        self.frage_liste.append(Frage("Wofür steht OOP? ", "objektorientierte programmierung"))
-        self.frage_liste.append(Frage("Was ist ein String? ", "zeichenkette"))
-        self.frage_liste.append(Frage("Was ist ein Integer? ", "Ganzahl"))
+        self.__option_hover = -1
+        self.__option_max = 1
+        self.__option_selected = -1
 
-    def get_random_question(self):
-        return random.choice(self.frage_liste)
+    def get_option_by_hovering(self):
+        return self.__option_hover
 
-    def check_freieantwort(self, antwort):
-        if antwort.strip() and not antwort.isdigit():
-            return antwort
-        else:
-            return "Bitte gib eine valide Antwort ein."
+    def increase_hover(self):
+        if self.__option_hover < self.__option_max - 1:
+            self.__option_hover += 1
 
-    def spielen(self):
-        print("Hallo Alice. Hier kannst du freie Antworten geben.")
-        frage_obj = self.get_random_question()
-        print(frage_obj.frage)
-        antwort = input("Was ist deine Antwort: ")
-        antwort = self.check_freieantwort(antwort)
-        print("Deine Antwort: " + antwort)
+    def decrease_hover(self):
+        if self.__option_hover > 0:
+            self.__option_hover -= 1
 
-        if antwort.lower() == frage_obj.antwort:
-            print("Deine Antwort ist korrekt!")
-        else:
-            print("Deine Antwort ist nicht korrekt. Die richtige Antwort ist:", frage_obj.antwort)
+    def get_option_max(self):
+        return self.__option_max
 
-if __name__ == "__main__":
-    spiel = FreiesAntwortSpiel()
-    spiel.spielen()
+    def set_option_max(self, new_max):
+        self.__option_max = new_max
+
+    def get_option_selected(self):
+        return self.__option_selected
+
+    def set_hover_as_selected(self):
+        self.__option_selected = self.__option_hover
+
+    def reset_selection(self):
+        self.__option_hover = -1
+        self.__option_selected = -1
